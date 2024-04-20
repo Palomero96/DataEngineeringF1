dfb85dc944b637b187a68536caa428312eb59a9e
David Palomero Méndez <31955373+Palomero96@users.noreply.github.com>
2024-04-20 15:55:36 +0200

    DataLoad
    python file to load data into Azure Postgres DataBase

---
+++ 4.LoadtoAzurePostgreSQL/LoadDataAzurePostgres.py
@@ -0,0 +1,66 @@
+# Two options
+# 1. Create table and use Copy command
+# 2. Load and create at the same time
+
+#Using Option number 2
+
+# Library
+import psycopg2
+import pandas as pd
+from sqlalchemy import create_engine, text
+
+# Define the database connection parameters
+db_params = {
+    'host': 'formula1serverdb.postgres.database.azure.com',
+    'database': 'formuladata',
+    'user': 'postgres',
+    'password': ''
+}
+
+# Create a connection to the PostgreSQL server
+conn = psycopg2.connect(
+    host=db_params['host'],
+    database=db_params['database'],
+    user=db_params['user'],
+    password=db_params['password']
+)
+
+#Create engine with SQLAlchemy
+engine= create_engine(f'postgresql://{db_params["user"]}:{db_params["password"]}@{db_params["host"]}/{db_params["database"]}')
+
+#Define path to data files
+path_to_files= r'C:\Users\palom\OneDrive\Documentos\GitHub\DataEngineeringF1\data'
+
+#Get all csv file paths
+csv_files = {
+    'circuits': path_to_files+"\circuits.csv",
+    'constructor_results': path_to_files+"\constructor_results.csv",
+    'constructor_standings': path_to_files+"\constructor_standings.csv",
+    'constructors': path_to_files+"\constructors.csv",
+    'driver_standings': path_to_files+"\driver_standings.csv",
+    'drivers': path_to_files+"\drivers.csv",
+    'lap_times': path_to_files+"\lap_times.csv",
+    'pit_stops': path_to_files+"\pit_stops.csv",
+    'qualifying': path_to_files+"\qualifying.csv",
+    'races': path_to_files+r"\races.csv",  #add "r" because of \r
+    'results': path_to_files+r"\results.csv", #add "r" because of \r
+    'seasons': path_to_files+"\seasons.csv",
+    'sprint_results': path_to_files+"\sprint_results.csv",
+    'status': path_to_files+"\status.csv"
+}
+
+# Check table structures
+""""
+for table_name, file_path in csv_files.items():
+    print(f"Contents of '{table_name}' CSV file:")
+    df = pd.read_csv(file_path)
+    print(df.head(1))  # Display the first few rows of the DataFrame
+    print("\n")
+"""
+
+# Use to_sql method to import tables to PostgreSQL from csv
+
+for table_name, file_path in csv_files.items():
+    df = pd.read_csv(file_path)
+    df.to_sql(table_name, engine, if_exists='replace', index=False)
+
