32557dec249af3f0acda563999c6e2e601da7498
palomero_96@hotmail.com <palomero_96@hotmail.com>
2024-04-22 12:19:50 +0000

    Data Transformation Update
    Clean code
    Retrieve secret value

--- Azure/databricks/dataTransformation.py
+++ Azure/databricks/dataTransformation.py
@@ -1,5 +1,6 @@
 # Databricks notebook source
-# Mount the Azure Data Lake Storaged
+# Mount the Azure Data Lake Storage
+client_secret = dbutils.secrets.get(scope="AzureKeyVault", key="databricksFormulaSecret")
 dbutils.fs.mount(
   source="abfss://formula1data@palomeroformula1data.dfs.core.windows.net", 
   mount_point="/mnt/formula1data",
@@ -7,54 +8,10 @@ dbutils.fs.mount(
     "fs.azure.account.auth.type": "OAuth",
     "fs.azure.account.oauth.provider.type": "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider",
     "fs.azure.account.oauth2.client.id": "a8c5f462-4cba-4c43-b82f-042126cfcb39",
-    "fs.azure.account.oauth2.client.secret": "",
-    "fs.azure.account.oauth2.client.endpoint": "https://login.microsoftonline.com/748cb327-cfee-46fb-8270-8c081cd60da3/oauth2/token"
-  }
-)
-
-# COMMAND ----------
-
-# Import the required libraries
-from azure.identity import ManagedIdentityCredential
-from azure.keyvault.secrets import SecretClient
-from pyspark.sql import SparkSession
-
-# Create a Spark session
-spark = SparkSession.builder.getOrCreate()
-
-# Set up the MSI credentials
-credential = ManagedIdentityCredential()
-
-# Create a SecretClient instance using the MSI credentials
-key_vault_url = "https://databricksSecretPalomero.vault.azure.net/"
-secret_client = SecretClient(vault_url=key_vault_url, credential=credential)
-
-# Get the secret value from the Key Vault
-client_id_secret = secret_client.get_secret("client-id")
-client_secret_secret = secret_client.get_secret("client-secret")
-tenant_id_secret = secret_client.get_secret("tenant-id")
-
-# Get the actual secret values
-client_id = client_id_secret.valueB
-client_secret = client_secret_secret.value
-tenant_id = tenant_id_secret.value
-
-# Mount the Azure Data Lake Storage
-dbutils.fs.mount(
-  source="abfss://formula1data@palomeroformula1data.dfs.core.windows.net",
-  mount_point="/mnt/formula1data",
-  extra_configs={
-    "fs.azure.account.auth.type": "OAuth",
-    "fs.azure.account.oauth.provider.type": "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider",
-    "fs.azure.account.oauth2.client.id": client_id,
     "fs.azure.account.oauth2.client.secret": client_secret,
     "fs.azure.account.oauth2.client.endpoint": "https://login.microsoftonline.com/748cb327-cfee-46fb-8270-8c081cd60da3/oauth2/token"
   }
 )
-
-# COMMAND ----------
-
-dbutils.secrets.get(scope="AzureKeyVault", key="databricksFormulaSecret")
 
 # COMMAND ----------
 
@@ -65,7 +22,6 @@ dbutils.secrets.get(scope="AzureKeyVault", key="databricksFormulaSecret")
 # COMMAND ----------
 
 #Load all files
-
 circuits = spark.read.format("csv").option("header", "true").option("inferSchema","true").load("/mnt/formula1data/raw-data-postgresAll/circuits.csv")
 constructor_results = spark.read.format("csv").option("header", "true").option("inferSchema","true").load("/mnt/formula1data/raw-data-postgresAll/constructor_results.csv") 
 constructor_standings = spark.read.format("csv").option("header", "true").option("inferSchema","true").load("/mnt/formula1data/raw-data-postgresAll/constructor_standings.csv")
@@ -84,10 +40,7 @@ status = spark.read.format("csv").option("header", "true").option("inferSchema",
 
 # COMMAND ----------
 
-
-
-# COMMAND ----------
-
+#CIRCUITS
 circuits.show()
 # Convert column types to double
 circuits = circuits.withColumn("alt", circuits["alt"].cast("integer"))
@@ -95,6 +48,7 @@ circuits.printSchema()
 
 # COMMAND ----------
 
+#CONSTRUCTOR_STANDINGS
 from pyspark.sql.functions import expr
 constructor_standings.show()
 constructor_standings = constructor_standings.withColumn("wins", expr("CASE WHEN wins > 0 THEN true ELSE false END"))
@@ -103,11 +57,13 @@ constructor_standings.printSchema()
 
 # COMMAND ----------
 
+#CONSTRUCTOR
 constructor.show()
 constructor = constructor.select("constructorId","constructorRef","nationality")
 
 # COMMAND ----------
 
+#DRIVER_STANDINGS
 driver_standings.show()
 driver_standings = driver_standings.withColumn("wins", expr("CASE WHEN wins > 0 THEN true ELSE false END"))
 driver_standings = driver_standings.select("driverStandingsId","raceId","driverId","points","position","wins")
@@ -115,6 +71,7 @@ driver_standings.printSchema()
 
 # COMMAND ----------
 
+#DRIVERS
 drivers.show()
 drivers = drivers.select("driverId","driverRef","code","forename","surname","dob","nationality")
 drivers = drivers.withColumnRenamed("dob", "birthDate")
@@ -122,40 +79,47 @@ drivers.printSchema()
 
 # COMMAND ----------
 
+#LAP_TIMES
 lap_times.show()
 lap_times.printSchema()
 
 # COMMAND ----------
 
+#PIT_STOPS
 pit_stops.show()
 pit_stops = pit_stops.drop("milliseconds")
 pit_stops.printSchema()
 
 # COMMAND ----------
 
+#QUALIFYING
 qualifying.show()
 qualifying.printSchema()
 
 # COMMAND ----------
 
+#RACES
 races.show()
 races = races.select("raceId","year","circuitId","date","time")
 races.printSchema()
 
 # COMMAND ----------
 
+#RESULTS
 results.show()
 results = results.drop("number","positionText","positionOrder","milliseconds")
 results.printSchema()
 
 # COMMAND ----------
 
+#SPRINT_RESULTS
 sprint_results.show()
 sprint_results = sprint_results.drop("number","positionText","positionOrder","milliseconds")
 sprint_results.printSchema()
 
 # COMMAND ----------
 
+#SEASONS and STATUS
 seasons.show()
 seasons.printSchema()
 status.show()
@@ -163,6 +127,7 @@ status.printSchema()
 
 # COMMAND ----------
 
+#Write dataframes to csv files
 circuits.write.format("csv").option("header", "true").mode("overwrite").save("/mnt/formula1data/transformed-data/circuits.csv")
 constructor_results.write.format("csv").option("header", "true").mode("overwrite").save("/mnt/formula1data/transformed-data/constructor_results.csv") 
 constructor_standings.write.format("csv").option("header", "true").mode("overwrite").save("/mnt/formula1data/transformed-data/constructor_standings.csv")
